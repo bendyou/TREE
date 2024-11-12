faf5a6f5ba0970c44a2300c920cfc9379467d6ae
bendyou <vladmambetov46@gmail.com>
2024-11-12 06:39:41 +0300

    tc_5_2

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Привет, добро пожаловать на сайт о деревьях!' });
 });
 
 router.get('/tree1', function(req, res, next) {
   res.render('tree', {
-  title: "Дерево",
-  picture: "images/tree1.png",
-  desc: "Ну типо описание первого дерева"
+    title: "Дерево тип 1",           // Заголовок страницы
+    picture: "/images/tree1.png",    // Путь к изображению
+    desc: "Описание первого дерева." // Описание дерева
   });
- });
+});
 
- router.get('/tree2', function(req, res, next) {
+router.get('/tree2', function(req, res, next) {
   res.render('tree', {
-  title: "Дерево",
-  picture: "images/tree2.png",
-  desc: "Ну типо описание первого дерева"
+    title: "Дерево тип 2",
+    picture: "/images/tree2.png",
+    desc: "Описание второго дерева."
   });
- });
+});
 
- router.get('/tree3', function(req, res, next) {
+router.get('/tree3', function(req, res, next) {
   res.render('tree', {
-  title: "Дерево",
-  picture: "images/tree3.png",
-  desc: "Ну типо описание первого дерева"
+    title: "Дерево тип 3",
+    picture: "/images/tree3.png",
+    desc: "Описание третьего дерева."
   });
- });
+});
 
 module.exports = router;
