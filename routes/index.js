25bfb314a6c7b444253106b968be6969fa786061
bendyou <vladmambetov46@gmail.com>
2024-11-12 05:28:24 +0300

    tc_4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/tree1', function(req, res, next) {
+  res.send("<h1>Дерево 1</h1>")
+});
+
+router.get('/tree2', function(req, res, next) {
+  res.send("<h1>Дерево 2</h1>")
+});
+
+router.get('/tree3', function(req, res, next) {
+  res.send("<h1>Дерево 3</h1>")
+});
+
 module.exports = router;
