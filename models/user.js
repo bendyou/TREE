9b86d1fc51603e0b819492647784ebe16b536fa8
bendyou <vladmambetov46@gmail.com>
2024-11-21 08:55:07 +0300

    tc_10_4

--- models/user.js
+++ models/user.js
@@ -20,6 +20,9 @@ type:Date,
 default: Date.now
 }
 })
+userSchema.methods.checkPassword = function(password){
+    return this.encryptPassword(password) === this.hashedPassword
+    }
 userSchema.virtual("password").set(function(password){
     this._purePassword = password
     this.salt = Math.random() + ""
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,48 @@
 var express = require('express');
 var router = express.Router();
 var Tree = require('../models/tree').Tree;
+var User = require('../models/user').User;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', counter:req.session.counter });
   });
 
-/* GET login/registration page. */
-router.get('/logreg', function(req, res, next) {
-  res.render('logreg',{title: 'Вход'});
-  var username = req.body.username
-  var password = req.body.password
-  console.log(username);
-  console.log(password);
-  });  
+  router.get('/logreg', function(req, res, next) {
+    res.render('logreg', { title: 'Вход' });
+});
+
+  /* GET login/registration page. */
+  router.get('/logreg', function(req, res, next) {
+    res.render('logreg',{title: 'Вход'});
+    });
+    
+  /* POST login/registration page. */
+  router.post('/logreg', async function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+    console.log(username);
+    console.log(password);
+    var users = await User.find({username: username});
+    console.log(users);
+    if (!users.length) {
+    //res.send("<h1>Пользователь НЕ найден</h1>");
+    var user = new
+    User({username:username,password:password})
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/');
+    } else {
+    //res.send("<h1>Пользователь найден</h1>");
+    var foundUser = users[0];
+    if(foundUser.checkPassword(password)){
+    req.session.user_id = foundUser._id
+    res.redirect('/')
+    } else {
+    res.render('logreg',{title: 'Вход'});
+    }
+    }
+    });
   
 router.get('/tree1', function(req, res, next) {
   res.render('tree', {
