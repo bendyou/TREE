121e982a08dfa4ef1966f3e06919f9a35c85e564
bendyou <vladmambetov46@gmail.com>
2024-11-21 09:08:18 +0300

    tc_10_6

--- routes/index.js
+++ routes/index.js
@@ -1,71 +1,41 @@
 var express = require('express');
 var router = express.Router();
-var Tree = require('../models/tree').Tree;
 var User = require('../models/user').User;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', counter:req.session.counter });
-  });
-
-  router.get('/logreg', function(req, res, next) {
-    res.render('logreg', { title: 'Вход' });
+  req.session.greeting = "Hi!!!";
+  res.render('index', { title: 'Express',  counter:req.session.counter  });
 });
 
-  /* GET login/registration page. */
-  router.get('/logreg', function(req, res, next) {
-    res.render('logreg',{title: 'Вход'});
-    });
-    
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход', error: null});
+  });
+
+
   /* POST login/registration page. */
-  router.post('/logreg', async function(req, res, next) {
-    var username = req.body.username
-    var password = req.body.password
-    console.log(username);
-    console.log(password);
-    var users = await User.find({username: username});
-    console.log(users);
-    if (!users.length) {
+router.post('/logreg', async function(req, res, next) {
+   var username = req.body.username
+   var password = req.body.password
+   var users = await User.find({username: username});
+   if (!users.length) {
     //res.send("<h1>Пользователь НЕ найден</h1>");
-    var user = new
-    User({username:username,password:password})
-    await user.save();
-    req.session.user_id = user._id;
-    res.redirect('/');
-    } else {
+    var user = new User({username:username,password:password})
+        await user.save();
+        req.session.user_id = user._id;
+        res.redirect('/');
+   } else {
     //res.send("<h1>Пользователь найден</h1>");
-    var foundUser = users[0];
-    if(foundUser.checkPassword(password)){
-    req.session.user_id = foundUser._id
-    res.redirect('/')
-    } else {
-    res.render('logreg',{title: 'Вход'});
-    }
-    }
-    });
+      var foundUser = users[0];
+      if(foundUser.checkPassword(password)){
+        req.session.user_id = foundUser._id
+        res.redirect('/')
+      } else {
+        res.render('logreg',{title: 'Вход', error: 'Пароль не верный'});
+      }
+   }   
+});
+
   
-router.get('/tree1', function(req, res, next) {
-  res.render('tree', {
-  title: "Дерево",
-  picture: "images/tree1.png",
-  desc: "Ну типо описание первого дерева"
-  });
- });
-
- router.get('/tree2', function(req, res, next) {
-  res.render('tree', {
-  title: "Дерево",
-  picture: "images/tree2.png",
-  desc: "Ну типо описание первого дерева"
-  });
- });
-
- router.get('/tree3', function(req, res, next) {
-  res.render('tree', {
-  title: "Дерево",
-  picture: "images/tree3.png",
-  desc: "Ну типо описание первого дерева"
-  });
- });
-
 module.exports = router;
