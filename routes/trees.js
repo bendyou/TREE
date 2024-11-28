bef2e0fe2bd0462263242706c008b6ddb4af337e
bendyou <vladmambetov46@gmail.com>
2024-11-28 10:32:02 +0300

    tc_10_8

--- routes/trees.js
+++ routes/trees.js
@@ -5,13 +5,13 @@ var checkAuth = require("../middlewares/checkAuth.js");
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с cats');
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с trees');
 });
 
 router.get("/:nick", checkAuth, async function(req, res, next) {
   var trees = await Tree.find({ nick: req.params.nick });
   console.log(trees)
-  if (!trees.length) return next(new Error("Нет такого котенка в мультфильме Три кота"))
+  if (!trees.length) return next(new Error("Нет такого дерева"))
   var tree = trees[0];
   res.render('tree', {
     title: tree.title,
